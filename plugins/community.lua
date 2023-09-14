7712426affb74966248906c4133f57d99ed0e31f
enrique.ruiz <enrique.ruiz@docplanner.com>
2023-09-14 15:38:10 +0200

    Add desired community packages

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,24 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
+  -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.typescript" },
 }
